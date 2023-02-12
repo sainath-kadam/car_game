84f80fd173e63beb13ce90aa9ceaf191152f444d
sainath-kadam <mydearsainathkadam@gmail.com>
2023-02-12 13:20:07 +0530

    commit

---
+++ code.js
@@ -0,0 +1,30 @@
+// setInterval(() => {
+//     let currentTime = Date.now();
+//     if (currentTime - startTime >= 5000) {
+//       startTime = currentTime;
+//       speed += 1;
+//     }
+    
+//   }, 1000);
+
+
+// let score = 0;
+
+// const myCar = {
+//     rearEndX: 50,
+//     rearEndY: 100
+//   };
+  
+//   const enemyCar = {
+//     rearEndX: 100,
+//     rearEndY: 100
+//   };
+  
+//   function updateScore() {
+//     if (myCar.rearEndX > enemyCar.rearEndX) {
+//       score += 10;
+//     }
+//     console.log(`Score: ${score}`);
+//   }
+  
+//   setInterval(updateScore, 100);
